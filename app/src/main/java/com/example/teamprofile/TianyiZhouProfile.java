a70849fef5499d2cb153c622869a9895a1b43da9
Admin <zhout17@mytru.ca>
2021-02-04 15:30:08 -0800

    adding title to main activity

--- app/src/main/java/com/example/teamprofile/TianyiZhouProfile.java
+++ app/src/main/java/com/example/teamprofile/TianyiZhouProfile.java
@@ -4,18 +4,13 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity
+public class TianyiZhouProfile extends AppCompatActivity
 {
 
-    /*
-    * Todo
-    *  - everyone of us have to create one separate activity(profile page)
-    *  -
-    * */
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_tianyi_zhou_profile);
     }
 }
